faca689680a56ad39fc29cb5e26795a3fa583716
jjosebastos <92466053+jjosebastos@users.noreply.github.com>
2024-08-20 15:08:56 -0300

    feat: ratings section

---
+++ CheckpointFront/src/Components/Banner/index.tsx
@@ -0,0 +1,30 @@
+
+
+const Banner : React.FC = () => {
+
+    return (
+        <>
+            <section className="banner">
+                <div className="game_title">
+                    <h1>
+                        God of War: RAGNAROK
+                    </h1>
+                </div>
+                <div className="game_ratings">
+                    <span>Avaliacao da ratings</span>
+                    <ul>
+                        <li> 9,6/10</li>
+                    </ul>
+                </div>
+                <div className="game_ratings">
+                    <span>Avaliacao da ratings</span>
+                    <ul>
+                        <li>avaliar</li>
+                    </ul>
+                </div>
+            </section>
+        </>
+    )
+};
+
+export default Banner;
--- CheckpointFront/src/Pages/Home/home.tsx
+++ CheckpointFront/src/Pages/Home/home.tsx
@@ -1,12 +1,13 @@
-import Header from "../Components/Header"
-import Footer from "../Components/Footer"
+import Header from "../../Components/Header";
+import Footer from "../../Components/Footer";
+import Banner from "../../Components/Banner"
 export default function Home(){
 
     return (
         <>
             <Header />
             <main>
-                
+                <Banner />
             </main>
             <Footer />
 

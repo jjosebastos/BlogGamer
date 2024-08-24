acd149b240763d2e0c142ba3df03e1e0373b93f1
Laboratório FIAP <labsfiap@fiap.com.br>
2024-08-23 22:28:53 -0300

    fix: dinamizando components

--- CheckpointFront/src/Components/TopRatings/index.tsx
+++ CheckpointFront/src/Components/TopRatings/index.tsx
@@ -3,57 +3,86 @@ import star from '../../assets/star.svg'
 import like from '../../assets/like.svg'
 import dislike from '../../assets/dislike.svg'
 import dotsVertical from '../../assets/dots-vertical.svg'
-const TopRatings : React.FC = () => {
+import toggleShift from '../../assets/toggle_shift.svg'
 
+type PropsTop = {
+    ratingTitle: string,
+    ratingNote: string,
+    ratingNoteFra: string,
+    username: string,
+    date: string,
+    ratingComment: string;
+    ratingLikes: string;
+    ratingDislikes: string;    
+};
+
+const TopRatings: React.FC<PropsTop> = ({
+    ratingTitle,
+    ratingNote,
+    ratingNoteFra,
+    username,
+    date,
+    ratingComment,
+    ratingLikes,
+    ratingDislikes
+}) => {
     return (
         <section className="top_ratings_container">
             <div className="top_container">
                 <div className="tag_container">
-                    <span>Avaliação em destaque</span>
+                    <span>{ratingTitle}</span>
                 </div>
                 <div className="star_ratings">
-                    <span><img src={star} alt="" />10/<span className='fraction'>10</span></span>
+                    <span>
+                        <img src={star} alt="Estrela" />
+                        {ratingNote}/{ratingNoteFra}
+                    </span>
                 </div>
             </div>
             <div className="container_username_comment">
                 <div className="username">
-                    <span>CaioHalbert</span>
+                    <span>{username}</span>
                 </div>
                 <div className="user_date_comment">
-                    <span>23 de agosto de 2024</span>
+                    <span>{date}</span>
                 </div>
             </div>
 
             <div className="user_article_container">
                 <div className="user_article_header">
-                    <h2>What an incredible sequel</h2>
+                    <div className="spoiler_container spoiler">
+                        <div className="warning_container">
+                            <h2 className='warning_header'>
+                                Warning: Spoilers!
+                            </h2>
+                        </div>
+                        <div className="warning_shift spoiler">
+                            <img src={toggleShift} alt="Seta" />
+                        </div>
+                    </div>
+                    <h2>{ratingTitle}</h2>
                     <br />
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, sint? Non explicabo vero repellat, harum temporibus repudiandae quae. Nobis ex quae sit delectus laborum adipisci eaque aliquam autem perspiciatis magnam.</p>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore incidunt maxime itaque, deleniti mollitia ea, nemo saepe earum, nulla vero obcaecati dicta neque fugiat voluptatibus voluptates suscipit maiores repellat quasi.</p>
+                    <p>{ratingComment}</p>    
                 </div>
             </div>
             <div className="container_interaction_icons">
                 <div className="interaction_icons">
                     <div className="interaction_like">
                         <img src={like} alt="Like botão" />
-                        <span className='interaction_value'>33</span>
+                        <span className='interaction_value'>{ratingLikes}</span>
                     </div>
                     <div className="interaction_dislike">
-                        <img src={dislike} alt="Deslike button" />
-                        <span className='interaction_value'>11</span>
+                        <img src={dislike} alt="Deslike botão" />
+                        <span className='interaction_value'>{ratingDislikes}</span>
                     </div>
-                    
                 </div>
                            
                 <div className="options_icon">
                     <span><img src={dotsVertical} alt="Options botão" /></span>
                 </div>
             </div>
-            
-
         </section>
     )
-
 }
 
 export default TopRatings
